0b2442d93451d8b6f1a16281f7a4d3c5c46d4661
Aleksandr Tkachenko <dn030475taa1@gmail.com>
2021-12-01 00:41:25 +0200

    init commit

--- src/com/pb/tkachenko/hw1/HelloWorld.java
+++ src/com/pb/tkachenko/hw1/HelloWorld.java
@@ -2,6 +2,6 @@ package com.pb.tkachenko.hw1;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World!");
     }
 }
